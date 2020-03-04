105c27c0be326ef9f7d3c367a4249dbb02105c4e
stellacheng-chm <setella.ming@gmail.com>
2020-03-04 15:44:47 +0800

    available for visitor to search

--- api/controllers/MaterialController.js
+++ api/controllers/MaterialController.js
@@ -15,12 +15,54 @@ module.exports = {
 //     return res.json(materials);
 // },
 
+materialsearch: async function (req, res) {
+    var models = await Book.find().sort([{id:'DESC'}]);
+    return res.view('material/materialsearch', { material: models});
+},
 
-    vmaterialsearch: async function (req, res) {
-
-    var models = await Material.find();
-    return res.view('material/vmaterialsearch', { materials: models });
+materialresult: async function(req, res){
+    const qmtlCatrgory=req.query.mtlcategory || "";
+    const qmtlname = req.query.mtlname;
+    const qmtlquantity = req.query.mtlquantity;
     
+    var models = await Book.find({
+        where:{
+        
+        mtlcategory:{contains:qCatrgory},
+        mtlname:{contains:qBookname},
+        mtlquantity:{contains:qAuthor},
+        }
+        
+    }).sort([{id:'DESC'}]);
+
+    return res.view('material/materialresult', {material:models});
+
+},
+
+vmaterialsearch: async function (req, res) {
+    var models = await Material.find().sort([{id:'DESC'}])
+    return res.view('material/vmaterialsearch', { material: models});
+},
+
+vmaterialresult: async function(req, res){
+    const qmtlCatrgory=req.query.mtlcategory || "";
+    const qmtlname = req.query.mtlname;
+    const qmtlquantity = req.query.mtlquantity;
+
+
+    var models = await material.find({
+        where:{
+        
+        mtlcategory:{contains:qmtlCatrgory},
+        mtlname:{contains:qmtlname},
+        quantity:{contains:qmtlquantity},
+
+        }
+        
+    }).sort([{id:'DESC'}]);
+
+    return res.view('book/vbookresult', {material:models});
+
 },
 
 
